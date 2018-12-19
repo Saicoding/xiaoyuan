784ecc8062eec54c7fd86d12846216582d375653
Saicoding <417353147@qq.com>
2018-12-19 16:15:19 +0800

    完成登录界面

---
+++ 小院/app.js
@@ -0,0 +1,92 @@
+//app.js
+App({
+  onLaunch: function() {
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+
+    wx.clearStorage();
+  },
+
+  /** 
+   * 自定义post函数，返回Promise
+   * +-------------------
+   * author: 武当山道士<912900700@qq.com>
+   * +-------------------
+   * @param {String}      url 接口网址
+   * @param {arrayObject} data 要传的数组对象 例如: {name: '武当山道士', age: 32}
+   * +-------------------
+   * @return {Promise}    promise 返回promise供后续操作
+   */
+  post: function(url, data, ifShow, ifCanCancel, title, pageUrl, ifGoPage, self) {
+
+    if (ifShow) {
+      wx.showLoading({
+        title: title,
+        mask: !ifCanCancel
+      })
+    }
+
+    var promise = new Promise((resolve, reject) => {
+
+      var that = this;
+      var postData = data;
+
+      //网络请求
+      wx.request({
+        url: url,
+        data: postData,
+        method: 'POST',
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        success: function(res) { //服务器返回数据
+          let status = res.data.code;
+          console.log(res)
+          let message = res.data.Message;
+          if (status == 1) { //请求成功
+            resolve(res);
+          } else if (status == -2) { //
+            wx.showToast({
+              icon:'none',
+              title: message,
+              duration:3000
+            })
+          } else if (status == -5) { //重复登录
+            console.log('重复登录')
+            wx.navigateTo({
+              url: '/pages/login1/login1?url=' + pageUrl + '&ifGoPage=' + ifGoPage
+            })
+          }
+          wx.hideLoading();
+        },
+        error: function(e) {
+          console.log('错误')
+          reject('网络出错');
+        }
+      })
+    });
+    return promise;
+  },
+
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ 小院/pages/login/login.js
@@ -0,0 +1,241 @@
+// pages/login/login.js
+let API_URL = "https://xcx2.chinaplat.com/xy/";
+let MD5 = require("../../common/MD5.js");
+let WXBizDataCrypt = require('../../utils/cryptojs/RdWXBizDataCrypt.js');
+let appId = "wxfc4ecbaf91acfaf6";
+let app = getApp();
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    showPwd:true,
+    openId: '', //用户唯一标识  
+    unionId: '',
+    encryptedData: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: "登录"
+    })
+    let self = this;
+    this.setData({
+      url: decodeURIComponent(options.url) == undefined ? "" : decodeURIComponent(options.url),
+      url1: options.url == undefined ? "" : options.url,
+      ifGoPage: options.ifGoPage == undefined ? false : options.ifGoPage
+    })
+
+    wx.login({
+      success: res => {
+        let code = res.code;
+        app.post(API_URL, "action=getSessionKey&code=" + code, false, false, "").then((res) => {
+          let sesstion_key = res.data.sessionKey;
+          let openid = res.data.openid;
+          console.log(sesstion_key)
+          console.log(openid)
+          self.setData({
+            sesstion_key: sesstion_key,
+            openid: openid
+          })
+        })
+      }
+    })
+
+    let rememberPwd = wx.getStorageSync('loginUser') ? true : false//如果没有本地缓存,就默认是不记住密码
+
+    if (rememberPwd){
+      let loginUser = wx.getStorageSync('loginUser');//缓存的用户名和密码
+      this.setData({
+        userText:loginUser.userText,
+        pwdText:loginUser.pwdText
+      })
+    }
+
+    this.setData({
+      rememberPwd: rememberPwd
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+  * 根据是否选择学校来处理登录逻辑
+  */
+  processSelectScholl: function (user, ifGoPage) {
+    let self = this;
+    let bindSchool = wx.getStorageSync('school');//是否绑定学校
+    if (bindSchool == "") {//如果没有绑定学校
+      self.setData({//将user设为本页面的变量，后续跳转到绑定学校页面需要用到
+        user: user
+      })
+      
+      wx.showModal({
+        title: '提示',
+        content: '请绑定您所在学校',
+        showCancel: false,
+        success(res) {
+          wx.navigateTo({//导航到选择学校页面
+            url: '/pages/selectSchool/selectSchool',
+          })
+        }
+      })
+    } else {//如果选择学校
+      wx.setStorage({
+        key: 'user',
+        data: user
+      })
+      wx.navigateBack({}) //先回到登录前的页面
+
+      if (ifGoPage == 'true') {
+        wx.navigateTo({
+          url: url,
+        })
+      }
+    }
+  },
+
+  /**
+   * 账号密码登录
+   */
+  login:function(){
+    let self = this;
+    let userText = self.data.userText;//当前输入的账户
+    let pwdText = self.data.pwdText;//当前输入的密码
+    let ifGoPage = self.data.ifGoPage //是否返回上一级菜单
+
+    let info = "";
+
+    if (userText == "" && pwdText == ""){
+      info = "用户名和密码不能为空"
+    } else if (userText == ""){
+      info = "用户名不能为空";
+    } else if(pwdText == ""){
+      info = "密码不能为空"
+    } else {
+      info = "未知错误"
+    }
+
+    if(info == ""){//如果输入不规范
+      wx.showToast({
+        icon:"none",
+        title: info,
+        duration:3000
+      })
+    }else{
+      let md5Pwd = MD5.md5(pwdText).toLowerCase();//小写md5加密密码
+
+      app.post(API_URL, "action=Login&user=" + userText + "&pwd=" + md5Pwd,true,false,"登录中").then(res=>{
+        let user = res.data.data[0];
+
+        let rememberPwd = self.data.rememberPwd;//是否记住密码
+        if (rememberPwd){//如果选择了记住密码就存储本地缓存
+          let loginUser = {
+            'userText': userText,
+            'pwdText':pwdText
+          }
+          wx.setStorage({
+            key: 'loginUser',
+            data: loginUser
+          })
+        }else{//如果没有选择记住密码就清除本地缓存
+          wx.removeStorage({
+            key: 'loginUser'
+          })
+        }
+
+        self.processSelectScholl(user, ifGoPage);
+ 
+      })
+    }
+  },
+
+  /**
+    * 微信授权登录
+    */
+  wxLogin: function (e) {
+    console.log(e)
+    let self = this;
+    let wxid = ""; //openId
+    let session_key = ""; //
+    let ifGoPage = self.data.ifGoPage //是否返回上一级菜单
+    let url = self.data.url; //需要导航的url
+
+    let encryptedData = e.detail.encryptedData;
+    let iv = e.detail.iv;
+    let signature = e.detail.signature; //签名
+    let nickname = e.detail.userInfo.nickName; //昵称
+    let headurl = e.detail.userInfo.avatarUrl; //头像
+    let sex = e.detail.userInfo.gender //性别
+    let code = self.data.code;
+
+    //得到openId和session_key
+
+    let sesstion_key = self.data.sesstion_key;
+    let openid = self.data.openid;
+
+    //拿到session_key实例化WXBizDataCrypt（）这个函数在下面解密用
+    let pc = new WXBizDataCrypt(appId, sesstion_key);
+    let data = pc.decryptData(encryptedData, iv);
+    let unionId = data.unionId;
+
+    app.post(API_URL, "action=LoginWx&unionId=" + unionId + "&openid=" + openid + "&nickname=" + nickname + "&headurl=" + headurl + "&sex=" + sex, true, false, "登录中").then((res) => {
+      let user = res.data.list[0];
+      self.processSelectScholl(user, ifGoPage);
+    })
+  },
+
+  /**
+   * 输入账号时存储变量
+   */
+  userInput:function(e){
+    this.setData({
+      userText: e.detail.value
+    })
+  },
+
+  /**
+   * 输入密码时存储变量
+   */
+  pwdInput:function(e){
+    this.setData({
+      pwdText: e.detail.value
+    })
+  },
+
+  /**
+   * 点击清除按钮
+   */
+  clearText:function(){
+    this.setData({
+      userText:""
+    })
+  },
+
+  /**
+   * 点击眼睛符号，改变是否显示密码
+   */
+  changeShowPwd:function(){
+    this.setData({
+      showPwd:!this.data.showPwd
+    })
+  },
+
+  /**
+   * 改变记录密码方式
+   */
+  changeRememberPwd:function(){
+    this.setData({
+      rememberPwd:!this.data.rememberPwd
+    })  
+  }
+})
