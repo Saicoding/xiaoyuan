408760e264e2f8c85379017df81a4ce6e291409a
Saicoding <417353147@qq.com>
2019-01-05 17:30:54 +0800

    活动首页与接口对接，更改样式

--- 小院/pages/huodong/huodong.js
+++ 小院/pages/huodong/huodong.js
@@ -9,68 +9,15 @@ Page({
    */
   data: {
     first: true, //第一次载入默认首次载入
-    banners: [
-      "https://xcx2.chinaplat.com/xy/images/1.png",
-      "/imgs/banner1.png",
-      "/imgs/banner2.png"
-    ],
+    zuixinSelected:true,//默认显示最新活动
+    index:-1//默认-1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    let user = wx.getStorageSync('user');
-    let pic = user.Pic;
-    let huodongs = [{
-        'src': '/imgs/banner1.png',
-        'title': '中仕学社JAVA软件工程师课程邀请您参加线下培训班',
-        'status': '报名中',
-        'info': '收费',
-        'headPics': [
-          pic, pic
-        ],
-        'num': 30,
-        'over': false
-      },
-      {
-        'src': '/imgs/banner2.png',
-        'title': '中仕学社大数据工程课程邀请您参加线下培训班',
-        'status': '已结束',
-        'info': '免费',
-        'headPics': [
-          pic, pic
-        ],
-        'num': 20,
-        'over': true
-      },
-      {
-        'src': '/imgs/head.jpg',
-        'title': 'PHP课程邀请您参加线下培训班',
-        'status': '已结束',
-        'info': '收费',
-        'headPics': [
-          pic, pic
-        ],
-        'num': 30,
-        'over': true
-      },
-      {
-        'src': '/imgs/card.jpg',
-        'title': '中仕学社自动化测试课程邀请您参加线下培训班',
-        'status': '报名中',
-        'info': '免费',
-        'headPics': [
-          pic, pic
-        ],
-        'num': 50,
-        'over': false
-      }
-    ]
 
-    this.setData({
-      huodongs: huodongs
-    })
   },
 
   /**
@@ -101,7 +48,6 @@ Page({
     if (isReLoad || first) {
       app.post(API_URL, "action=getIndexPic&loginrandom=" + loginrandom + "&zcode=" + zcode, false, false, "", "", "", self).then(res => {
         let banners = res.data.data;
-        console.log(banners)
         self.setData({
           banners: banners
         })
@@ -110,6 +56,7 @@ Page({
       // // 获取活动话题分类
       app.post(API_URL, "action=getActivityTypes&loginrandom=" + loginrandom + "&zcode=" + zcode, false, false, "", "", "", self).then(res => {
         let types = res.data.data;
+        console.log(types)
         self.setData({
           types:types,
         })
@@ -118,34 +65,74 @@ Page({
       // 获取活动列表
       console.log("action=getActivityList_new&loginrandom=" + loginrandom + "&zcode=" + zcode)
       app.post(API_URL, "action=getActivityList_new&loginrandom=" + loginrandom + "&zcode=" + zcode, false, false, "", "", "", self).then(res => {
-        console.log(res)
+        let page_all = res.data.data[0].page_all;
+        let huodongs = res.data.data[0].list;
+
+        self.initHuodongs(huodongs)
+
         self.setData({
-          first:false
+          first:false,
+          page_all: page_all,
+          page:1,
+          huodongs: huodongs
         })
       })
     }
   },
 
-
-
+  initHuodongs: function (huodongs){
+    for (let i = 0; i < huodongs.length; i++) {
+      let huodong = huodongs[i];
+      switch (huodong.zhuangtai) {
+        case "1":
+          huodong.zhuangtaiStr = "报名中"
+          break;
+        case "2":
+          huodong.zhuangtaiStr = "进行中"
+          break;
+        case "3":
+          huodong.zhuangtaiStr = "已结束"
+          break;
+        default:
+          console.log('获取活动状态出错')
+          break;
+      }
+    }
+  },
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
-
+  onUnload: function (huodongs) {
   },
 
   /**
    * 导航到社团页面
    */
-  GOshetuan: function(e) {
+  getList: function(e) {
+    let self = this;
     let typesid = e.currentTarget.dataset.typesid;
-    if (buttonClicked) return;
-    buttonClicked = true;
-    console.log(typesid)
-    wx.navigateTo({
-      url: '/pages/huodong/shetuan/shetuan?typesid=' + typesid,
-    })
+    let index = e.currentTarget.dataset.index !=undefined ? e.currentTarget.dataset.index:-1;
+
+    let types = self.data.types;
+
+    for (let i = 0; i < types.length; i++) {
+      types[i].selected = false;
+    }
+
+    if(index !=-1){//点击了除了最新活动的按钮
+      types[index].selected = true;
+      self.setData({
+        zuixinSelected:false,
+        types:types,
+        index: index
+      })
+    }else{
+      self.setData({
+        zuixinSelected: true,
+        types: types,
+        index: index
+      })
+    }
   },
 
   /**
