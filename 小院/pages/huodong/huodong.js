adb6d165df0bcb3a34386507f6e79fe3e7e0b18d
Saicoding <417353147@qq.com>
2019-01-07 10:18:44 +0800

    处理活动页面点击分类的逻辑
    点击其中一个分类，先判断是不是第一次载入，如果第一次载入就请求接口，请求接口成功后保存请求结果到本地缓存，如果不是第一次载入，就不请求接口，数据使用本地缓存

--- 小院/pages/huodong/huodong.js
+++ 小院/pages/huodong/huodong.js
@@ -10,7 +10,10 @@ Page({
   data: {
     first: true, //第一次载入默认首次载入
     zuixinSelected:true,//默认显示最新活动
-    index:-1//默认-1
+    index:-1,//默认-1
+    loadingMore: false, //是否在加载更多
+    loadingText: "", //上拉载入更多的文字
+    isLoaded:false,//是否已经加载
   },
 
   /**
@@ -63,7 +66,6 @@ Page({
       })
 
       // 获取活动列表
-      console.log("action=getActivityList_new&loginrandom=" + loginrandom + "&zcode=" + zcode)
       app.post(API_URL, "action=getActivityList_new&loginrandom=" + loginrandom + "&zcode=" + zcode, false, false, "", "", "", self).then(res => {
         let page_all = res.data.data[0].page_all;
         let huodongs = res.data.data[0].list;
@@ -72,6 +74,7 @@ Page({
 
         self.setData({
           first:false,
+          isLoaded:true,
           page_all: page_all,
           page:1,
           huodongs: huodongs
@@ -99,6 +102,63 @@ Page({
       }
     }
   },
+
+  /**
+ * 页面上拉触底事件的处理函数
+ */
+  onReachBottom: function () {
+    let self = this;
+    let loadingMore = self.data.loadingMore;
+    if (loadingMore) return; //如果还在载入中,就不继续执行
+
+    let types = self.data.types;
+    let index = self.data.index;
+
+    let page_all = self.data.page_all;
+    let page = self.data.page;
+
+    let typesid = types[index]== undefined ? "" : types[index].id ;
+
+    console.log(typesid)
+    if (page >= page_all) {
+      self.setData({ //正在载入
+        loadingText: "没有了..."
+      })
+      return;
+    }
+
+    self.setData({ //正在载入
+      showLoadingGif: true,
+      loadingText: "载入更多活动..."
+    })
+
+    //用户信息
+    let user = wx.getStorageSync('user');
+    let loginrandom = user.Login_random;
+    let zcode = user.zcode;
+
+    let huodongs = self.data.huodongs;
+    page++;
+
+    app.post(API_URL, "action=getActivityList_new&loginrandom=" + loginrandom + "&zcode=" + zcode + "&page=" + page+"&typesid="+typesid, false, false, "", "", "", self).then(res => {
+      let newHuodongs = res.data.data[0].list;
+      huodongs = huodongs.concat(newHuodongs);
+
+      self.setData({
+        showLoadingGif: false,
+        loadingText: "载入完成"
+      })
+
+      setTimeout(function () {
+        self.setData({
+          page: page,
+          loadingMore: false,
+          huodongs: huodongs,
+          loadingText: ""
+        })
+      }, 200)
+    })
+  },
   /**
    * 生命周期函数--监听页面卸载
    */
@@ -112,6 +172,9 @@ Page({
     let self = this;
     let typesid = e.currentTarget.dataset.typesid;
     let index = e.currentTarget.dataset.index !=undefined ? e.currentTarget.dataset.index:-1;
+    let user = wx.getStorageSync('user');
+    let loginrandom = user.Login_random;
+    let zcode = user.zcode;
 
     let types = self.data.types;
 
@@ -124,8 +187,30 @@ Page({
       self.setData({
         zuixinSelected:false,
         types:types,
-        index: index
+        index:index
       })
+      if (types[index].first){//如果已经载入一次就return
+        let huodongs = wx.getStorageSync('type' + types[index].id + zcode);//本地缓存
+        self.setData({
+          huodongs: huodongs
+        })
+      }else{
+        self.setData({
+          isLoaded: false
+        })
+      }
+      //开始请求
+      app.post(API_URL, "action=getActivityList_new&loginrandom=" + loginrandom + "&zcode=" + zcode + "&typesid=" + typesid, false, false, "", "", "", self).then(res => {
+        let huodongs = res.data.data[0].list;
+        types[index].first = true;//设置分类已经有第一次载入
+        wx.setStorageSync('type' + types[index].id + zcode, huodongs);//本地缓存
+        self.setData({
+          huodongs: huodongs,
+          isLoaded: true,
+          types: types
+        })
+      })
+
     }else{
       self.setData({
         zuixinSelected: true,
@@ -136,29 +221,11 @@ Page({
   },
 
   /**
-   * 导航到优惠页面
-   */
-  GOyouhui: function() {
-    if (buttonClicked) return;
-    buttonClicked = true;
-    wx.navigateTo({
-      url: '/pages/huodong/youhui/youhui',
-    })
-  },
-
-  /**
    * 导航到详情页
    */
   GOhuodongDetail: function() {
     wx.navigateTo({
       url: '/pages/huodong/huodongDetail/huodongDetail',
     })
-  },
-
-  /**
-   * 导航到社团或者优惠界面
-   */
-  GOshetuanOryouhui: function() {
-
   }
 })
