417b4be2b29437d55c09325805f10ffe7f14005e
Saicoding <417353147@qq.com>
2019-01-21 17:18:38 +0800

    完成账户中心的积分记录，学币记录，学时记录页面

---
+++ 小院/pages/mine/zhanghuzhongxin/xueshiRecord/xueshiRecord.js
@@ -0,0 +1,110 @@
+// pages/mine/zhanghuzhongxin/jifenRecord/jifenRecord.js
+let app = getApp();
+let API_URL = "https://xcx2.chinaplat.com/xy/";
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    loadingMore: false, //是否在加载更多
+    loadingText: "", //上拉载入更多的文字
+    showLoadingGif: false, //是否显示刷新gif图
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '学时收支记录',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    let self = this;
+    let user = wx.getStorageSync('user');
+    let loginrandom = user.Login_random;
+    let zcode = user.zcode;
+
+    app.post(API_URL, "action=getXueshiRecords&loginrandom=" + loginrandom + "&zcode=" + zcode + "&page=1", false, false, "", "", "", self).then(res => {
+      let result = res.data.data[0];
+      let list = result.list;
+      let page_all = result.page_all;
+      console.log(list)
+      self.setData({
+        list: list,
+        page_all: page_all,
+        page: 1
+      })
+    })
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    let self = this;
+    let loadingMore = self.data.loadingMore;
+    if (loadingMore) return; //如果还在载入中,就不继续执行
+
+    let allpage = self.data.page_all;
+    let page = self.data.page;
+
+    if (page >= allpage) {
+      self.setData({ //正在载入
+        loadingText: "别扯了,我是有底线的..."
+      })
+      return;
+    }
+
+    self.setData({ //正在载入
+      showLoadingGif: true,
+      loadingText: "载入更多记录 ..."
+    })
+
+    //用户信息
+    let user = wx.getStorageSync('user');
+    let loginrandom = user.Login_random;
+    let zcode = user.zcode;
+
+    let list = self.data.list;
+    page++;
+
+    app.post(API_URL, "action=getXueshiRecords&loginrandom=" + loginrandom + "&zcode=" + zcode + "&page=" + page, false, false, "", "", "", self).then(res => {
+      let newlist = res.data.data[0].list;
+      list = list.concat(newlist);
+
+      self.setData({
+        showLoadingGif: false,
+        loadingText: "载入完成"
+      })
+
+      setTimeout(function () {
+        self.setData({
+          page: page,
+          loadingMore: false,
+          list: list,
+          loadingText: ""
+        })
+      }, 200)
+    })
+  },
+})
--- 小院/pages/mine/zhanghuzhongxin/zhanghuzhongxin.js
+++ 小院/pages/mine/zhanghuzhongxin/zhanghuzhongxin.js
@@ -1,4 +1,8 @@
 // pages/mine/zhanghuzhongxin/zhanghuzhongxin.js
+let app = getApp();
+let API_URL = "https://xcx2.chinaplat.com/xy/";
+let md5 = require('../../../common/MD5.js');
+let buttonClicked = false;
 Page({
 
   /**
@@ -11,7 +15,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     wx.setNavigationBarTitle({
       title: '账户中心',
     })
@@ -21,49 +25,178 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    let self = this;
+    let user = wx.getStorageSync('user');
+    self.setData({
+      jifen: user.Jifen,
+      xuebi: user.Money
+    })
   },
 
   /**
-   * 生命周期函数--监听页面隐藏
+   * 微信支付输入框
    */
-  onHide: function () {
-
+  wxpayInput: function(e) {
+    let currentValue = e.detail.value;
+    let valueStr = currentValue.toString();
+    if (valueStr[0] == '.') { //如果第一个输入的是.
+      wx.showToast({
+        icon: 'none',
+        title: '输入金额错误',
+        duration: 3000
+      })
+      this.setData({
+        lastvalue: ''
+      })
+      return;
+    } else if (valueStr.length >= 4 && currentValue < 0.01) { //如果输入金额是一分钱
+      let lastvalue = this.data.lastvalue;
+      wx.showToast({
+        icon: 'none',
+        title: '充值金额不能小于0.01元',
+        duration: 3000
+      })
+      this.setData({
+        lastvalue: lastvalue
+      })
+    } else if (valueStr.indexOf('.')<valueStr.length-3) { //如果金额大于4
+      let lastvalue = this.data.lastvalue;
+      wx.showToast({
+        icon: 'none',
+        title: '充值金额最小单位为分',
+        duration: 3000
+      })
+      this.setData({
+        lastvalue: lastvalue
+      })
+    } else { //正常赋值
+      this.setData({
+        lastvalue: currentValue
+      })
+    }
   },
 
   /**
-   * 生命周期函数--监听页面卸载
+   * 导航到积分记录
    */
-  onUnload: function () {
-
+  GOjifen: function() {
+    wx.navigateTo({
+      url: '/pages/mine/zhanghuzhongxin/jifenRecord/jifenRecord',
+    })
   },
 
   /**
-   * 页面相关事件处理函数--监听用户下拉动作
+   * 导航到积分记录
    */
-  onPullDownRefresh: function () {
-
+  GOxuebi: function() {
+    wx.navigateTo({
+      url: '/pages/mine/zhanghuzhongxin/xuebiRecord/xuebiRecord',
+    })
   },
 
   /**
-   * 页面上拉触底事件的处理函数
+   * 导航到积分记录
    */
-  onReachBottom: function () {
-
+  GOxueshi: function() {
+    wx.navigateTo({
+      url: '/pages/mine/zhanghuzhongxin/xueshiRecord/xueshiRecord',
+    })
   },
 
   /**
-   * 用户点击右上角分享
+   * 支付
    */
-  onShareAppMessage: function () {
+  /**
+  * 支付
+  */
+  pay: function (e) {
+    if (buttonClicked) return;
+    buttonClicked = true;
 
+    let self = this;
+    let type = e.currentTarget.dataset.type; //付款方法
+    let lastvalue = self.data.lastvalue;
+    let user = wx.getStorageSync('user');
+    let loginrandom = user.Login_random;
+    let zcode = user.zcode;
+
+    if (type == "chongzhika") {
+      app.post(API_URL, "action=BuyVIP&zcode=" + zcode + "&loginrandom=" + loginrandom + "&years=" + index, false, false, "", "", "", self).then(res => {
+
+        let vip_jtime = res.data.data[0].vip_jtime;
+        let vip_ktime = res.data.data[0].vip_ktime;
+
+        wx.showModal({
+          title: '提示',
+          content: '恭喜您,充值成功,VIP到期时间：' + vip_jtime
+        })
+
+        self.setData({ //更新截止时间
+          Vip: '1',
+          Jtime: vip_jtime,
+          Ktime: vip_ktime,
+        })
+      })
+    } else if (type == "wx") {
+      // 登录
+      wx.login({
+        success: res => {
+          // 发送 res.code 到后台换取 openId, sessionKey, unionId
+          let code = res.code;
+          app.post(API_URL, "action=getSessionKey&code=" + code, true, false, "购买中").then((res) => {
+            let openid = res.data.openid;
+            app.post(API_URL, "action=unifiedorder&loginrandom=" + loginrandom + "&zcode=" + zcode + "&product=" + lastvalue + "&openid=" + openid, true, false, "购买中").then((res) => {
+              let status = res.data.code;
+              console.log('ok')
+              if (status == "1") {
+                let timestamp = Date.parse(new Date());
+                timestamp = timestamp / 1000;
+                timestamp = timestamp.toString();
+                let nonceStr = "TEST";
+                let prepay_id = res.data.prepay_id;
+                let appId = "wxfc4ecbaf91acfaf6";
+                let myPackage = "prepay_id=" + prepay_id;
+                let key = "e625b97ae82c3622af5f5a56d1118825";
+
+                let str = "appId=" + appId + "&nonceStr=" + nonceStr + "&package=" + myPackage + "&signType=MD5&timeStamp=" + timestamp + "&key=" + key;
+                let paySign = md5.md5(str).toUpperCase();
+
+                let myObject = {
+                  'timeStamp': timestamp,
+                  'nonceStr': nonceStr,
+                  'package': myPackage,
+                  'paySign': paySign,
+                  'signType': "MD5",
+                  success: function (res) {
+                    app.post(API_URL,"action=getUserInfo&loginrandom=" + loginrandom + "&zcode=" + zcode,false,false,"","","",self).then(res=>{
+                      let xuebi = self.data.xuebi
+                      let userInfo = res.data.data[0];
+                      self.setData({
+                        xuebi: parseFloat(userInfo.Money) + parseFloat(xuebi),
+                        lastvalue:''
+                      })
+                    })
+                  },
+                  fail: function (res) {
+                    console.log(res)
+                  }
+                }
+                wx.requestPayment(myObject)
+              }
+            })
+          })
+        }
+      })
+    }
   }
+
+
 })
